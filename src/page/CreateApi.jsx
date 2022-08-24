0774c024c533b26072ecdeb786c221f916a8b260
mumei404 <lozld30@gmail.com>
2022-08-25 02:15:00 +0900

    create api, step3

--- src/page/CreateApi.jsx
+++ src/page/CreateApi.jsx
@@ -1,32 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 export const CreateApi = () => {
-	const { register, handleSubmit, watch, formState: { isSubmitSuccessful, errors }, getValues, setValue, reset } = useForm();
+	const { register, handleSubmit, formState: { isSubmitSuccessful, errors }, reset, control } = useForm();
+	const { fields, append, remove } = useFieldArray({
+		control,
+		name: 'schemaItems'
+	})
 	const navigate = useNavigate();
+
+	// https://codesandbox.io/s/react-hook-form-handlesubmit-with-reset-xrh0t?file=/src/index.js
 	const [submittedData, setSubmittedData] = useState({})
 	const onSubmit = data => {
-		setSubmittedData({ ...data, step: step + 1})
+		let nextData = { ...data, step: step + 1}
+		setSubmittedData(nextData)
 		if(data.step === 3) {
-			console.log('final')
+			console.log('final', nextData)
 		}
 	};
 	const back = e => {
 		setSubmittedData({ ...submittedData, step: submittedData.step - 1 })
 	};
-	let step = submittedData.step || 1;
 	useEffect(() => {
 		if(isSubmitSuccessful) {
 			reset({ ...submittedData })
 		}
 	}, [isSubmitSuccessful, submittedData, reset])
 
+	let step = submittedData.step || 1;
+
 	return (
 		<>
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<input type="hidden" value={step + 1} {...register("step")} />
 				{
+					// https://react-hook-form.com/get-started
 					step == 1 && (
 						<>
 							<section>
@@ -64,11 +73,49 @@ export const CreateApi = () => {
 					)
 				}
 				{
+					// https://react-hook-form.com/api/usefieldarray
 					step == 3 && (
 						<>
-							<section>
-						hogehogehoge
-							</section>
+							<ul>
+								{fields.map((item, index) => {
+									return (
+									<section key={item.id}>
+										<li>
+											<label htmlFor={`schemaItems[${index}].fieldId`}>フィールドID</label>
+											<input
+												{...register(`schemaItems[${index}].fieldId`, {required: true})}
+												defaultValue={`${item.fieldId}`}
+											/>
+											<label htmlFor={`schemaItems[${index}].displayName`}>表示名</label>
+											<input
+												{...register(`schemaItems[${index}].displayName`, {required: true})}
+												defaultValue={`${item.displayName}`}
+											/>
+											<label htmlFor={`schemaItems[${index}].type`}>種類</label>
+											<select {...register(`schemaItems[${index}].type`, {required: true})}>
+												{['テキストフィールド', 'テキストエリア', '画像', 'セレクトフィールド'].map(selectValue => (
+													<option key={selectValue} value={selectValue}>
+														{selectValue}
+													</option>
+												))}
+											</select>
+
+											<button type="button" onClick={() => remove(index)}>
+												削除
+											</button>
+										</li>
+									</section>
+									);
+								})}
+							</ul>
+							<button
+								type="button"
+								onClick={() => {
+									append({ fieldId: "", displayName: "", type: "テキストフィールド" });
+								}}
+							>
+								フィールドを追加
+							</button>
 						</>
 					)
 				}
